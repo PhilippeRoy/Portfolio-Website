29de3a4f54494372ac9038e07379a63c64af0da7
Philippe Roy <philippe.j.roy@gmail.com>
2014-11-26 16:23:32 +1100

    Gulp file testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
 
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+gulp.task('sass', function() {
+    gulp.src(__dirname + '/sass/stylesheets/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest(__dirname + '/public/stylesheets/'));
+  });
+
+
+gulp.task('default', ['sass']);

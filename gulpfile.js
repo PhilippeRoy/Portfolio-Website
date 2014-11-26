93ba9e42f64f8c81919b22895ed773917c90943f
Philippe Roy <philippe.j.roy@gmail.com>
2014-11-26 16:51:09 +1100

    Gulp file testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,31 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-minify-css');
 
 
 gulp.task('sass', function() {
     gulp.src(__dirname + '/sass/stylesheets/*.scss')
-    .pipe(sass())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
     .pipe(gulp.dest(__dirname + '/public/stylesheets/'));
   });
 
 
-gulp.task('default', ['sass']);
+gulp.task('autoprefixer', function () {
+    gulp.src(__dirname + '/public/stylesheets/*.css')
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(gulp.dest(__dirname + '/public/stylesheets/'));
+  });
+
+
+gulp.task('minify-css', function() {
+   gulp.src(__dirname + '/public/stylesheets/*.css')
+   .pipe(minifyCSS({keepBreaks:false}))
+   .pipe(gulp.dest(__dirname + '/public/stylesheets/'));
+  });
+
+gulp.task('default', ['sass','autoprefixer', 'minify-css']);

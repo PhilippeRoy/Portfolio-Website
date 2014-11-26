350168d838e3d799131d8ddf578cdfea16b05e46
Philippe Roy <philippe.j.roy@gmail.com>
2014-11-26 15:25:46 +1100

    A lot of stuff

--- app.js
+++ app.js
@@ -18,12 +18,12 @@ app.engine('html', require('hbs').__express);
 hbs.registerPartials(__dirname + '/views/partials');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
- 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);

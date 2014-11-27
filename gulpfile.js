b7612e7c7669beebdfb4d3ebce677833f12b926f
Philippe Roy <philippe.j.roy@gmail.com>
2014-11-27 11:15:30 +1100

    ...

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-    minifyCSS = require('gulp-minify-css');
+    minifyCSS = require('gulp-minify-css'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
 
 
+// CSS
 gulp.task('sass', function() {
     gulp.src(__dirname + '/sass/stylesheets/*.scss')
     .pipe(sass({
@@ -28,4 +31,17 @@ gulp.task('minify-css', function() {
    .pipe(gulp.dest(__dirname + '/public/stylesheets/'));
   });
 
-gulp.task('default', ['sass','autoprefixer', 'minify-css']);
+// JS
+gulp.task('concat-scripts', function() {
+    gulp.src(__dirname + '/public/javascripts/*.js')
+    .pipe(concat('bundle.min.js', {newLine: ';'}))
+    .pipe(gulp.dest(__dirname + '/public/javascripts/'))
+  });
+
+gulp.task('compress', function() {
+    gulp.src(__dirname + '/public/javascripts/bundle.min.js')
+    .pipe(uglify())
+    .pipe(gulp.dest(__dirname + '/public/javascripts/production/'))
+  });
+
+gulp.task('default', ['sass','autoprefixer', 'minify-css', 'concat-scripts', 'compress']);
